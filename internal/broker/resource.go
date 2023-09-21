8654e7a274bda4678c7ed9b03a9a96e4f6dbc569
Balazs Czoma <balazs.czoma@solace.com>
2023-09-21 11:22:19 -0400

    Code improvement in comparing default values

--- internal/broker/resource.go
+++ internal/broker/resource.go
@@ -24,7 +24,6 @@ import (
 	"net/http"
 	"net/url"
 	"path/filepath"
-	"reflect"
 	"strings"
 
 	"github.com/hashicorp/terraform-plugin-framework/diag"
@@ -76,27 +75,28 @@ func isValueEqualsAttrDefault(attr *AttributeInfo, response tftypes.Value, broke
 	if err != nil {
 		return false, err
 	}
-	defaultValue := attr.Default
-	if defaultValue == nil {
+	if attr.Default == nil {
 		if brokerDefault.IsNull() {
 			// No broker default
 			return false, nil
 		}
 		// Analyze broker default
-		def, err := attr.Converter.FromTerraform(brokerDefault)
+		brokerDefaultValue, err := attr.Converter.FromTerraform(brokerDefault)
 		if err != nil {
 			return false, err
 		}
 		// compare
-		return responseValue == def, nil
+		return responseValue == brokerDefaultValue, nil
 	}
-	if attr.BaseType == Int64 {
-		if reflect.ValueOf(defaultValue).Kind() == reflect.Float64 {
-			return responseValue == int64(defaultValue.(float64)), nil
-		}
-		return defaultValue.(int) == int(responseValue.(int64)), nil
+	tfDefault, err := attr.Converter.ToTerraform(attr.Default)
+	if err != nil {
+		return false, err
 	}
-	return fmt.Sprintf("%v", defaultValue) == fmt.Sprintf("%v", responseValue), nil
+	attrDefaultValue, err := attr.Converter.FromTerraform(tfDefault)
+	if err != nil {
+		return false, err
+	}
+	return responseValue == attrDefaultValue, nil
 }
 
 func toId(path string) string {
