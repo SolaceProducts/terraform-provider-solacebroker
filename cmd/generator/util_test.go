2f8d294eb46159eca2f8a8b66653a66298dc3685
Balazs Czoma <balazs.czoma@solace.com>
2024-06-04 09:23:16 -0400

    Acceptance test fix

--- cmd/generator/util_test.go
+++ cmd/generator/util_test.go
@@ -26,6 +26,7 @@ func TestCliParamsWithEnv(t *testing.T) {
 		cliParams CliParams
 	}
 	url := "https://localhost:1943"
+	bearerToken := "abc"
 	tests := []struct {
 		name string
 		args args
@@ -37,7 +38,7 @@ func TestCliParamsWithEnv(t *testing.T) {
 					Url:                      &url,
 					Username:                 nil,
 					Password:                 nil,
-					Bearer_token:             nil,
+					Bearer_token:             &bearerToken,
 					Retries:                  nil,
 					Retry_min_interval:       nil,
 					Retry_max_interval:       nil,
